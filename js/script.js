703f121301e4488834eef73be68dec0142f1ce09
Ashot0 <illia.holovan@gmail.com>
2023-05-25 23:56:47 +0300

    burger

--- js/script.js
+++ js/script.js
@@ -56,6 +56,13 @@ if (menuLinks.length > 0) {
 		if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
 			const gotoBlock = document.querySelector(menuLink.dataset.goto);
 			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+
+			window.scrollTo({
+				top: gotoBlockValue,
+				behavior: "smooth"
+			});
+			e.preventDefault();
+
 		}
 	}
 }
