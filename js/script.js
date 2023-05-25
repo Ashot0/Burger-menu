5a7e606dec7a6c42453a1f8d1a22b6bb1fa53c58
Ashot0 <illia.holovan@gmail.com>
2023-05-26 00:37:03 +0300

    complete

--- js/script.js
+++ js/script.js
@@ -37,32 +37,45 @@ if (isMobile.any()) {
 				menuArrow.parentElement.classList.toggle(`_active`);
 			});
 		}
-
 	}
 } else {
 	document.body.classList.add('_pc');
 }
 
-// Прокрутка
 
+// Меню бургер
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__bodu');
+if (iconMenu) {
+	iconMenu.addEventListener("click", function (e) {
+		document.body.classList.toggle('._lock');
+		iconMenu.classList.toggle('_active');
+		menuBody.classList.toggle('_active');
+	});
+}
+
+// Прокрутка
 const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
 if (menuLinks.length > 0) {
 	menuLinks.forEach(menuLinks => {
 		menuLinks.addEventListener("click", onMenuLinkClick);
 	});
-
 	function onMenuLinkClick(e) {
 		const menuLink = e.target;
 		if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
 			const gotoBlock = document.querySelector(menuLink.dataset.goto);
 			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
-
+			if (iconMenu.classList.contains('_active')) {
+				document.body.classList.remove('._lock');
+				iconMenu.classList.remove('_active');
+				menuBody.classList.remove('_active');
+			}
 			window.scrollTo({
 				top: gotoBlockValue,
 				behavior: "smooth"
 			});
 			e.preventDefault();
-
 		}
 	}
 }
+
